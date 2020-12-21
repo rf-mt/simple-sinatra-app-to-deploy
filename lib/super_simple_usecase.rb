028cae91223235b17c7a005e4deab3365871583f
rebecca fitzsimmons <rebecca@madetech.com>
2020-12-21 19:34:33 +0000

    Console log url in usecase

--- lib/super_simple_usecase.rb
+++ lib/super_simple_usecase.rb
@@ -1,5 +1,7 @@
 class SuperSimpleUsecase
   def execute(url)
+    puts 'in usecase!'
+    puts 'url'
     return "I'm deployed to Heroku!" if url.include? "herokuapp"
 
     'Deploy me to Heroku, please!'
