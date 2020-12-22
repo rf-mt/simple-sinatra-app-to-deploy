84f6a9cb2bfc8b10f2777ea04f27d637d9468f95
rebecca fitzsimmons <rebecca@madetech.com>
2020-12-22 08:16:48 +0000

    Combine test and deploy workflow in circle build, and only deploy if tests pass

--- lib/super_simple_usecase.rb
+++ lib/super_simple_usecase.rb
@@ -1,7 +1,5 @@
 class SuperSimpleUsecase
   def execute(url)
-    puts 'in usecase!'
-    puts 'url'
     return "I'm deployed to Heroku!" if url.include? "herokuapp"
 
     'Deploy me to Heroku, please!'
